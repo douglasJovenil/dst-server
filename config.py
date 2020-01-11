dc65ba62534b69221ed5740ddc0dffc936a6f208
Douglas Jovenil <douglascastilhos@yahoo.com.br>
2020-01-11 17:32:37 -0300

    Modificado config.py para Debian

--- config.py
+++ config.py
@@ -6,7 +6,14 @@ def main():
     system('sudo apt-get update -y')
     system('sudo apt-get upgrade -y')
     system('sudo apt-get autoremove -y')
-    system('sudo apt-get install docker.io -y')
+    
+    system('sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y')
+    system('curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -')
+    system('sudo apt-key fingerprint 0EBFCD88')
+    system('sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"')
+    system('sudo apt-get update -y')
+    system('sudo apt-get install docker-ce docker-ce-cli containerd.io')
+
     system('sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose')
     system('sudo chmod +x /usr/local/bin/docker-compose')
     system('sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose')
