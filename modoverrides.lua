00e0597e397fb8f2dfade83b4305176b6b8fdd63
Douglas Jovenil <douglascastilhos@yahoo.com.br>
2020-01-12 19:29:10 -0300

    .

--- modoverrides.lua
+++ modoverrides.lua
@@ -1,18 +1,18 @@
 return {
-    -- ["workshop-1207269058"]={ configuration_options={  }, enabled=true },
-    -- ["workshop-374550642"]={ configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
-    -- ["workshop-378160973"]={
-    --   configuration_options={
-    --     ENABLEPINGS=true,
-    --     FIREOPTIONS=2,
-    --     OVERRIDEMODE=false,
-    --     SHAREMINIMAPPROGRESS=true,
-    --     SHOWFIREICONS=true,
-    --     SHOWPLAYERICONS=true,
-    --     SHOWPLAYERSOPTIONS=2 
-    --   },
-    --   enabled=true 
-    -- },
-    -- ["workshop-478005098"]={ configuration_options={  }, enabled=true },
-    -- ["workshop-501385076"]={ configuration_options={ quick_harvest=true }, enabled=true } 
+    ["workshop-1207269058"]={ configuration_options={  }, enabled=true },
+    ["workshop-374550642"]={ configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
+    ["workshop-378160973"]={
+      configuration_options={
+        ENABLEPINGS=true,
+        FIREOPTIONS=2,
+        OVERRIDEMODE=false,
+        SHAREMINIMAPPROGRESS=true,
+        SHOWFIREICONS=true,
+        SHOWPLAYERICONS=true,
+        SHOWPLAYERSOPTIONS=2 
+      },
+      enabled=true 
+    },
+    ["workshop-478005098"]={ configuration_options={  }, enabled=true },
+    ["workshop-501385076"]={ configuration_options={ quick_harvest=true }, enabled=true } 
   }
