2c00203179673c254080446b365e96f078ccbe70
Douglas Jovenil <douglascastilhos@yahoo.com.br>
2020-06-09 03:39:44 -0300

    removidos arquivos do mundo antigo

--- overworld/overworld/modoverrides.lua
+++
@@ -1,7 +0,0 @@
-return {
-  ["workshop-1207269058"] = { enabled = true },
-  ["workshop-478005098"] = { enabled = true },
-  ["workshop-378160973"] = { enabled = true },
-  ["workshop-374550642"] = { enabled = true },
-  ["workshop-501385076"] = { enabled = true },
-}
