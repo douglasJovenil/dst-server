02c468fc7322abf91e118fe0a3f32e215a2861e3
Douglas Jovenil <douglascastilhos@yahoo.com.br>
2020-06-13 04:03:26 -0300

    add: mods

--- modoverrides.lua
+++ modoverrides.lua
@@ -17,6 +17,7 @@ return {
     ["workshop-501385076"]={ configuration_options={ quick_harvest=true }, enabled=true },
     ["workshop-478005098"]={ configuration_options={  }, enabled=true },
     ["workshop-661253977"]={ configuration_options={  }, enabled=true },
-    ["workshop-569043634"]={ configuration_options={  }, enabled=true }
+    ["workshop-569043634"]={ configuration_options={  }, enabled=true },
+    ["workshop-1862534310"]={ configuration_options={  }, enabled=true }
   }
-  }
+}
