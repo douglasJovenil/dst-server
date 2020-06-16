481619401f4b2bc9cba55d6efc6aab6d713febdd
Douglas Jovenil <douglascastilhos@yahoo.com.br>
2020-06-16 00:47:40 -0300

    update: docker-compose.yml

--- modoverrides.lua
+++ modoverrides.lua
@@ -15,7 +15,6 @@ return {
   },
   ['workshop-478005098'] =  { enabled=true },
   ['workshop-501385076'] =  { configuration_options={ quick_harvest=true }, enabled=true },
-  ['workshop-478005098'] =  { enabled=true },
   ['workshop-661253977'] =  { enabled=true },
   ['workshop-569043634'] =  { enabled=true },
   ['workshop-1862534310'] = { enabled=true },
