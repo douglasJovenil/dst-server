0e3da0cf88f5d048f149c07fdd18b922dabf65d1
Douglas Jovenil <douglascastilhos@yahoo.com.br>
2020-06-16 00:35:45 -0300

    update: modoverrides.lua

--- modoverrides.lua
+++ modoverrides.lua
@@ -1,23 +1,22 @@
 return {
-    ["workshop-1207269058"]={ enabled=true },
-    ["workshop-374550642"]={ configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
-    ["workshop-378160973"]={
-      configuration_options={
-        ENABLEPINGS=true,
-        FIREOPTIONS=2,
-        OVERRIDEMODE=false,
-        SHAREMINIMAPPROGRESS=true,
-        SHOWFIREICONS=true,
-        SHOWPLAYERICONS=true,
-        SHOWPLAYERSOPTIONS=2 
-      },
-      enabled=true 
+  ["workshop-1207269058"]={ enabled=true },
+  ["workshop-374550642"]={ configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
     },
-    ["workshop-478005098"]={ enabled=true },
-    ["workshop-501385076"]={ configuration_options={ quick_harvest=true }, enabled=true },
-    ["workshop-478005098"]={ enabled=true },
-    ["workshop-661253977"]={ enabled=true },
-    ["workshop-569043634"]={ enabled=true },
-    ["workshop-1862534310"]={ enabled=true }
-  }
+    enabled=true 
+  },
+  ["workshop-478005098"]={ enabled=true },
+  ["workshop-501385076"]={ configuration_options={ quick_harvest=true }, enabled=true },
+  ["workshop-478005098"]={ enabled=true },
+  ["workshop-661253977"]={ enabled=true },
+  ["workshop-569043634"]={ enabled=true },
+  ["workshop-1862534310"]={ enabled=true },
 }
