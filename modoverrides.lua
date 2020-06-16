e8ac246f4a89452d94df3843caf56c57182617bd
Douglas Jovenil <douglascastilhos@yahoo.com.br>
2020-06-16 00:37:29 -0300

    update: modoverrides.lua

--- modoverrides.lua
+++ modoverrides.lua
@@ -1,7 +1,7 @@
 return {
-  ["workshop-1207269058"]={ enabled=true },
-  ["workshop-374550642"]={ configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
-  ["workshop-378160973"]={
+  ['workshop-1207269058'] = { enabled=true },
+  ['workshop-374550642'] =  { configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
+  ['workshop-378160973'] =  {
     configuration_options={
       ENABLEPINGS=true,
       FIREOPTIONS=2,
@@ -13,10 +13,10 @@ return {
     },
     enabled=true 
   },
-  ["workshop-478005098"]={ enabled=true },
-  ["workshop-501385076"]={ configuration_options={ quick_harvest=true }, enabled=true },
-  ["workshop-478005098"]={ enabled=true },
-  ["workshop-661253977"]={ enabled=true },
-  ["workshop-569043634"]={ enabled=true },
-  ["workshop-1862534310"]={ enabled=true },
+  ['workshop-478005098'] =  { enabled=true },
+  ['workshop-501385076'] =  { configuration_options={ quick_harvest=true }, enabled=true },
+  ['workshop-478005098'] =  { enabled=true },
+  ['workshop-661253977'] =  { enabled=true },
+  ['workshop-569043634'] =  { enabled=true },
+  ['workshop-1862534310'] = { enabled=true },
 }
