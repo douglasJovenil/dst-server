cd29b264c0a4b5f01e4ee167befe21e4d5403283
Douglas J. C. Santos <douglascastilhos@yahoo.com.br>
2021-01-15 22:22:42 -0300

    Create simplemodoverrides.lua

---
+++ container/simplemodoverrides.lua
@@ -0,0 +1,17 @@
+return {
+  ['workshop-1207269058'] = { enabled=true },
+  ['workshop-374550642'] =  { configuration_options={ MAXSTACKSIZE=99 }, enabled=true },
+  ['workshop-378160973'] =  {
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ['workshop-501385076'] =  { configuration_options={ quick_harvest=true }, enabled=true },
+}
